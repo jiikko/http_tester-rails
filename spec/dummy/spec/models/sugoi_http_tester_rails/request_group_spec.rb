70328ca22ac3762afba36cec1936dbcb8b0b0934
jiikko <n905i.1214@gmail.com>
2016-09-13 08:09:19 +0900

    fix spec

--- spec/dummy/spec/models/sugoi_http_tester_rails/request_group_spec.rb
+++ spec/dummy/spec/models/sugoi_http_tester_rails/request_group_spec.rb
@@ -10,26 +10,24 @@ describe SugoiHttpTesterRails::RequestGroup do
     request_group = testing_host.request_groups.create!
 
     success_results = 20.times.map do
-      {
-        path:        '/',
+      { path:        '/',
         device_type: :sp,
         method: 'GET',
         status_code: 200 }
     end
     server_error_resullts = 20.times.map do
-      {
-        path:        '/',
+      { path:        '/',
         device_type: :sp,
         method: 'GET',
         status_code: 500 }
     end
-    allow_any_instance_of(template_request_group.class).to receive(:separate_run).and_return(
+    allow_any_instance_of(template_request_group.class).to receive(:separation_run).and_return(
       server_error_resullts + success_results
     )
     request_group.run_http_test!(template_request_group: template_request_group)
     expect(request_group.requests.count).to eq 1
 
-    allow_any_instance_of(template_request_group.class).to receive(:separate_run).and_return(
+    allow_any_instance_of(template_request_group.class).to receive(:separation_run).and_return(
       success_results + server_error_resullts
     )
     request_group.run_http_test!(template_request_group: template_request_group)
