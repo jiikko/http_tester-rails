72492a0ee6a4679fd0e773e882e35f2f0730be58
jiikko <n905i.1214@gmail.com>
2016-09-14 23:49:45 +0900

    project をwebから登録できるようにする

--- app/controllers/sugoi_http_tester_rails/projects_controller.rb
+++ app/controllers/sugoi_http_tester_rails/projects_controller.rb
@@ -7,4 +7,23 @@ class SugoiHttpTesterRails::ProjectsController < ApplicationController
     @project = SugoiHttpTesterRails::Project.find(params[:id])
     @template_request_groups = SugoiHttpTesterRails::TemplateRequestGroup.order(id: :desc)
   end
+
+  def new
+    @project = SugoiHttpTesterRails::Project.new
+  end
+
+  def create
+    @project = SugoiHttpTesterRails::Project.new(project_param)
+    if @project.save
+      redirect_to @project, notice: 'プロジェクトを作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_param
+    params.required(:project).permit(:name)
+  end
 end
