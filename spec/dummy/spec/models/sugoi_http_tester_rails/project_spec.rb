a3eeb1f3808d44173e209a065f0480658f236d74
jiikko <n905i.1214@gmail.com>
2016-10-07 22:38:28 +0900

    重複してログを登録しないようにする

--- spec/dummy/spec/models/sugoi_http_tester_rails/project_spec.rb
+++ spec/dummy/spec/models/sugoi_http_tester_rails/project_spec.rb
@@ -7,7 +7,9 @@ describe SugoiHttpTesterRails::Project do
       begin
         log = <<-LOG
 {"method":"GET","user_agent":"sugou_http_request_tester 0.0.1","path":"/index.html","mt":"GET","ua":"ddd","pt":"/index.html","device_type":"pc"}
+{"method":"GET","user_agent":"sugou_http_request_tester 0.0.1","path":"/index.html","mt":"GET","ua":"ddd","pt":"/index.html","device_type":"pc"}
 {"method":"GET","user_agent":"sugou_http_request_tester 0.0.1","path":"/index2.html","mt":"GET","ua":"ddd","pt":"/index2.html","device_type":"pc"}
+{"method":"GET","user_agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36 sugou_http_request_tester 0.0.1","path":"/index3.html","mt":"GET","ua":"Mobile","pt":"/index3.html","device_type":"sp"}
 {"method":"GET","user_agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36 sugou_http_request_tester 0.0.1","path":"/index3.html","mt":"GET","ua":"Mobile","pt":"/index3.html","device_type":"sp"}
         LOG
         File.write(file.path, log)
