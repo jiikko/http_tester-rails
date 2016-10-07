c2f279fc7ea7687cada332482e3aa42b830e62d5
jiikko <n905i.1214@gmail.com>
2016-10-07 22:50:24 +0900

    revert 重複してログを登録しないようにする

--- app/models/sugoi_http_tester_rails/project.rb
+++ app/models/sugoi_http_tester_rails/project.rb
@@ -1,19 +1,4 @@
 class SugoiHttpTesterRails::Project < ActiveRecord::Base
-  class CreatedStore
-    def initialize
-      @table = {}
-    end
-
-    def stored?(hash)
-      @table[hash[:path]] && @table[hash[:path]][hash[:device_type]]
-    end
-
-    def <<(hash)
-      @table[hash[:path]] || (@table[hash[:path]] = {})
-      @table[hash[:path]][hash[:device_type]] = true
-    end
-  end
-
   has_many :testing_hosts, dependent: :destroy
   has_many :template_request_groups, dependent: :destroy
 
@@ -34,12 +19,8 @@ class SugoiHttpTesterRails::Project < ActiveRecord::Base
     }
     tester.import_logs!
     list = tester.export_request_list!(export_format: :array)[0..80_000] # 適当に8万できる
-    created_hash = {}
-    created_store = CreatedStore.new # 作成済みのレコードをオンメモリでチェックする
     request_group = template_request_groups.create!
     list.each do |hash|
-      next if created_store.stored?(hash)
-      created_store << hash # 今は不要だけど念のため
       request_group.template_requests.create!(
         device_type: hash[:device_type],
         http_method: SugoiHttpTesterRails::TemplateRequest::HTTP_METHOD_TABLE[hash[:method]] || next, # 知らないメソッドがきたらnext
